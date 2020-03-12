c9ad1e41e584abb62290f9814bf888ada4e03630
sarafotros <sarafotros@gmail.com>
2020-03-12 12:00:12 +0000

    forms change

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,11 +3,7 @@ import React from 'react';
 const Main = () => {
     return (
 			<div>
-				<img
-					src="https://picsum.photos/200/300
-"
-					alt="logo"
-				/>
+				<img src="https://picsum.photos/200/300" alt="logo" />
 			</div>
 		);
 }
