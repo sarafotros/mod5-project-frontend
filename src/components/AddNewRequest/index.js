0f44ca4f50cbe387b94045d10eabd871e04e03ae
sarafotros <sarafotros@gmail.com>
2020-03-15 00:13:55 +0000

    routing issues resoled, upload photo resolverd, post request added, service description added, localStorage

--- src/components/AddNewRequest/index.js
+++ src/components/AddNewRequest/index.js
@@ -5,47 +5,41 @@ import LocationForm from './location';
 import UploadData from './Upload';
 import * as moment from 'moment';
 
-
+const URLUpload = 'http://localhost:3001/upload-photos';
 
 const levels = [
-	{
-		title: 'description',
-		value: 'DESCRIPTION'
-	},
-	{
-		title: 'timePicker',
-		value: 'TIME_PICKER'
-	},
-	{
-		title: 'location',
-		value: 'LOCATION_FORM'
-	},
-	{
-		title: 'upload data',
-		value: 'UPLOAD_DATA'
-	}
+  {
+    title: 'description',
+    value: 'DESCRIPTION',
+  },
+  {
+    title: 'timePicker',
+    value: 'TIME_PICKER',
+  },
+  {
+    title: 'location',
+    value: 'LOCATION_FORM',
+  },
+  {
+    title: 'upload data',
+    value: 'UPLOAD_DATA',
+  },
 ];
 
 class AddNewRequest extends React.Component {
 	state = {
 		currentLevel: 0,
-		body: {
-			coordinates: [-0.0874836, 51.5202116],
-			number: '',
-			postCode: '',
-			selectedHour: {
-				id: 5,
-				value: '12:00'
-			},
-			selectedDay: {
-				id: 1,
-				value: moment().format('MMMM Do')
-			}
-		}
+		coordinates: { lng: -0.0874836, lat: 51.5202116 },
+		number: '',
+		postCode: '',
+		selectedHour: '12:00',
+		selectedDay: moment().format('YYYY-MM-DD'),
+		description: '',
+    image: '',
+    imageName:''
 	};
 
-
-    // taking states from child components
+	// taking states from child components
 	changeCoordinates = newCoordinates => {
 		this.setState({
 			coordinates: newCoordinates
@@ -62,19 +56,32 @@ class AddNewRequest extends React.Component {
 			postCode: newValue
 		});
 	};
-	changeSelectedDay = newValue => {
+	changeSelectedDay = event => {
 		this.setState({
-			selectedDay: newValue
+			selectedDay: event.target.value
 		});
 	};
-	changeSelectedHour = newValue => {
+	changeSelectedHour = event => {
 		this.setState({
-			selectedHour: newValue
+			selectedHour: event.target.value
 		});
-    };
-    
+	};
 
-    // next page button 
+	changePhotoDescription = event => {
+		this.setState({
+			description: event.target.value
+		});
+	};
+
+	changeImage = event => {
+    console.log(event.target.files)
+    this.setState({
+      image: event.target.files[0],
+      imageName: event.target.value
+		});
+	};
+
+	// next page button
 	goNextPage = () => {
 		const { currentLevel } = this.state;
 		this.setState({ currentLevel: currentLevel + 1 });
@@ -85,6 +92,44 @@ class AddNewRequest extends React.Component {
 		this.setState({ currentLevel: currentLevel - 1 });
 	};
 
+  /// Submit request
+  
+    // t.bigint "user_id", null: false
+    // t.bigint "service_id", null: false
+    // t.date "date"
+    // t.time "time"
+    // t.integer "number"
+    // t.string "post_code"
+    // t.text "description"
+
+  submitRequest = () => {
+    const {
+			image,
+			description,
+			selectedDay,
+			selectedHour,
+			number,
+			postCode
+		} = this.state;
+    const formData = new FormData();
+    formData.append('photo', image)
+    formData.append('description', description);
+    formData.append('date', selectedDay);
+    formData.append('time', selectedHour);
+    formData.append('number', number);
+    formData.append('post_code', postCode);
+		formData.append('user_id', localStorage.user_id);
+		formData.append('service_id', JSON.parse(localStorage.selectedService).id);
+
+		return fetch(URLUpload, {
+			method: 'POST',
+			headers: {},
+			body: formData
+		})
+			.then(resp => resp.json())
+			.then(data => console.log(data.image_url));
+	};
+
 	render() {
 		console.log('adding new request');
 		const {
@@ -93,8 +138,12 @@ class AddNewRequest extends React.Component {
 			number,
 			postCode,
 			selectedDay,
-			selectedHour
+			selectedHour,
+			description,
+      image,
+      imageName
 		} = this.state;
+		console.log('selectedDay', selectedDay);
 		return (
 			<div>
 				{levels[currentLevel].value === 'DESCRIPTION' && <Description />}
@@ -108,25 +157,43 @@ class AddNewRequest extends React.Component {
 				)}
 				{levels[currentLevel].value === 'LOCATION_FORM' && (
 					<LocationForm
-						changeCoordinates={this.changeCoordinates}
-						changeNumber={this.changeNumber}
-						changePostCode={this.changePostCode}
 						number={number}
 						postCode={postCode}
 						coordinates={coordinates}
+						changeCoordinates={this.changeCoordinates}
+						changeNumber={this.changeNumber}
+						changePostCode={this.changePostCode}
+						// these props are required for using react-google-maps package
+						googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+						loadingElement={<div style={{ height: `300px`, width: '50%' }} />}
+						containerElement={<div style={{ height: `100%`, width: '50%' }} />}
+						mapElement={<div style={{ height: `300px`, width: '50%' }} />}
 					/>
 				)}
-				{levels[currentLevel].value === 'UPLOAD_DATA' && <UploadData />}
+				{levels[currentLevel].value === 'UPLOAD_DATA' && (
+					<UploadData
+						description={description}
+            image={image}
+            imageName={imageName}
+						changePhotoDescription={this.changePhotoDescription}
+						changeImage={this.changeImage}
+					/>
+				)}
 				<div>
 					<button disabled={currentLevel === 0} onClick={this.goPrevPage}>
 						Back
 					</button>
-					<button
-						disabled={currentLevel === levels.length - 1}
-						onClick={this.goNextPage}
-					>
-						Next
-					</button>
+					{currentLevel !== levels.length - 1 && (
+						<button
+							disabled={currentLevel === levels.length - 1}
+							onClick={this.goNextPage}
+						>
+							Next
+						</button>
+					)}
+					{currentLevel === levels.length - 1 && (
+						<button onClick={this.submitRequest}>Submit</button>
+					)}
 				</div>
 			</div>
 		);
--- src/components/AddNewRequest/location.js
+++ src/components/AddNewRequest/location.js
@@ -1,89 +1,85 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import {
+  GoogleMap,
+  Marker,
+  withGoogleMap,
+  withScriptjs,
+} from 'react-google-maps';
+import { withRouter } from 'react-router-dom';
 
-function getCoordinates(address) {
-	fetch(
-		'https://maps.googleapis.com/maps/api/geocode/json?address=' +
-			address +
-			'&key=' +
-			'AIzaSyAqrwNRHTXGSnwdwg_cUYVwvK4vvsnk06c'
-	)
-		.then(response => response.json())
-		.then(data => {
-			const latitude = data.results.geometry.location.lat;
-			const longitude = data.results.geometry.location.lng;
-			console.log({ latitude, longitude });
-		});
+// function getCoordinates(address) {
+// 	fetch(
+// 		'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+// 			address +
+// 			'&key=' +
+// 			'AIzaSyAqrwNRHTXGSnwdwg_cUYVwvK4vvsnk06c'
+// 	)
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			const latitude = data.results.geometry.location.lat;
+// 			const longitude = data.results.geometry.location.lng;
+// 			console.log({ latitude, longitude });
+// 		});
+// }
+
+class LocationForm extends Component {
+  //   onMapClick = (e, f) => {
+  //     const { changeCoordinates } = this.props;
+  //     const longitude = f.lngLat.lng;
+  //     const latitude = f.lngLat.lat;
+  //     const coord = [longitude, latitude];
+  //     changeCoordinates(coord);
+  //   };
+
+  // componentDidMount() {
+  // 	const postCode = 'ec2a 1nt';
+  // 	getCoordinates(postCode);
+  // }
+
+  render() {
+    const {
+      coordinates,
+      number,
+      postCode,
+      changeNumber,
+      changePostCode,
+      changeCoordinates,
+    } = this.props;
+    return (
+      <div>
+        <div>
+          <label>Number</label>
+          <input
+            type='text'
+            value={number}
+            onChange={(e) => changeNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>PostCode</label>
+          <input
+            type='text'
+            value={postCode}
+            onChange={(e) => changePostCode(e.target.value)}
+          />
+        </div>
+        <div>
+          <GoogleMap
+            defaultZoom={8}
+            defaultCenter={coordinates}
+            onClick={(event) =>
+              changeCoordinates({
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+              })
+            }
+          >
+            <Marker position={coordinates} />
+          </GoogleMap>
+        </div>
+      </div>
+    );
+  }
 }
 
-const Map = ReactMapboxGl({
-	accessToken:
-		'pk.eyJ1IjoiaGFtaWRrMTAzMCIsImEiOiJjazdwMzltaDUwNGJyM2ducmpsY2Z3MGJoIn0.rZgrXUTVKe8ThAs5mYWyQA'
-});
-
-export default class LocationForm extends Component {
-	onMapClick = (e, f) => {
-		const { changeCoordinates } = this.props;
-		const coord = [];
-		const longitude = f.lngLat.lng;
-		const latitude = f.lngLat.lat;
-		coord.push(longitude, latitude);
-		changeCoordinates([longitude, latitude]);
-	};
-
-	componentDidMount() {
-		// const postCode = 'ec2a 1nt';
-		// getCoordinates(postCode);
-	}
-
-	render() {
-		const {
-			coordinates,
-			number,
-			postCode,
-			changeNumber,
-			changePostCode
-		} = this.props;
-		return (
-			<div>
-				<div>
-					<label>Number</label>
-					<input
-						type="text"
-						value={number}
-						onChange={e => changeNumber(e.target.value)}
-					/>
-				</div>
-				<div>
-					<label>PostCode</label>
-					<input
-						type="text"
-						value={postCode}
-						onChange={e => changePostCode(e.target.value)}
-					/>
-				</div>
-				<div>
-					<Map
-						style="mapbox://styles/mapbox/streets-v9"
-						containerStyle={{
-							height: '50vh',
-							width: '50vw'
-						}}
-						onClick={this.onMapClick}
-						center={coordinates}
-					>
-						<Layer
-							type="symbol"
-							id="marker"
-							layout={{ 'icon-image': 'marker-15' }}
-							onMapClick={this.onMapClick}
-						>
-							<Feature coordinates={coordinates} />
-						</Layer>
-					</Map>
-				</div>
-				;
-			</div>
-		);
-	}
-}
+export default withScriptjs(withGoogleMap(LocationForm));
