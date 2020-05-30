562dd0ef1ea9c316771e51cfc0c7949603baf6bf
sarafotros <sarafotros@gmail.com>
2020-05-30 12:07:44 +0100

    netlify resolved

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
     return (
         <div>
-            {/* <img src='https://httpstatusdogs.com/img/404.jpg' alt='not found'/> */}
+            <img src='https://httpstatusdogs.com/img/404.jpg' alt='not found'/>
         </div>
     )
 }
